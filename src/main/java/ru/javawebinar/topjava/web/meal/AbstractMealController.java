89a7ac269aed66c37a31a4480fac10cd9bd452c9
Urban Aleksandr <urban.aleksandr@gmail.com>
2017-11-13 17:58:28 +0200

    init spring context, inject MealRestController

--- src/main/java/ru/javawebinar/topjava/web/meal/AbstractMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/AbstractMealController.java
@@ -3,7 +3,6 @@ package ru.javawebinar.topjava.web.meal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 
@@ -18,30 +17,30 @@ public abstract class AbstractMealController {
     @Autowired
     private MealService service;
 
-    public List<Meal> getAll() {
-        log.info("getAll by user with id={}", AuthorizedUser.id());
-        return service.getAll(AuthorizedUser.id());
+    public List<Meal> getAll(int userId) {
+        log.info("getAll by user with id={}", userId);
+        return service.getAll(userId);
     }
 
-    public Meal get(int id) {
-        log.info("get {} by user with id={}", id, AuthorizedUser.id());
-        return service.get(id, AuthorizedUser.id());
+    public Meal get(int id, int userId) {
+        log.info("get {} by user with id={}", id, userId);
+        return service.get(id, userId);
     }
 
-    public Meal create(Meal meal) {
-        log.info("create {} by user with id={}", meal, AuthorizedUser.id());
+    public Meal create(Meal meal, int userId) {
+        log.info("create {} by user with id={}", meal, userId);
         checkNew(meal);
-        return service.create(meal, AuthorizedUser.id());
+        return service.create(meal, userId);
     }
 
-    public void delete(int id) {
-        log.info("delete {} by user with id={}", id, AuthorizedUser.id());
-        service.delete(id, AuthorizedUser.id());
+    public void delete(int id, int userId) {
+        log.info("delete {} by user with id={}", id, userId);
+        service.delete(id, userId);
     }
 
-    public void update(Meal meal, int id) {
-        log.info("update {} with id={} by user with id={}", meal, id, AuthorizedUser.id());
+    public void update(Meal meal, int id, int userId) {
+        log.info("update {} with id={} by user with id={}", meal, id, userId);
         assureIdConsistent(meal, id);
-        service.update(meal, AuthorizedUser.id());
+        service.update(meal, userId);
     }
 }
